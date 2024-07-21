b4b85c696de45ac44b3bc43c71a89e05c5796d15
omerglck <omergolcek10@gmail.com>
2024-07-22 00:44:52 +0300

    update

--- src/store/actions/charactersActions.js
+++ src/store/actions/charactersActions.js
@@ -1,9 +1,13 @@
 import {CHARACTERS_URL} from '../../service/urls';
 import {getRequest} from '../../service/verbs';
 import {
+  CHANGE_PARAMS,
   CHARACTERS_REJECT,
   FETCH_CHARACTERS,
+  FETCH_SINGLECHARACTER,
   PENDING_CHARACTERS,
+  PENDING_SINGLECHARACTER,
+  SINGLECHARACTER_REJECT,
 } from '../types/characterTypes';
 
 export const getCharacterList = params => {
@@ -18,5 +22,27 @@ export const getCharacterList = params => {
     } catch (error) {
       dispatch({type: CHARACTERS_REJECT, error: error});
     }
+  };
+};
+
+export const getSingleCharacter = characterID => {
+  const url = `${CHARACTERS_URL}/${characterID}`;
+
+  return async dispatch => {
+    dispatch({type: PENDING_SINGLECHARACTER});
+    try {
+      const response = await getRequest(url);
+      dispatch({
+        type: FETCH_SINGLECHARACTER,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({type: SINGLECHARACTER_REJECT, error: error});
+    }
+  };
+};
+export const changeParams = params => {
+  return async dispatch => {
+    dispatch({type: CHANGE_PARAMS, params: params});
   };
 };
--- src/theme/colors.js
+++ src/theme/colors.js
@@ -3,5 +3,8 @@ const Colors = {
   BLACK: '#000000',
   WHITE: '#FFFFFF',
   BACKTITLECOLOR: '#f3f4f4',
+  GREEN: '#5fc992',
+  RED: '#fd1d00',
+  BROWN: '#7f7558',
 };
 export default Colors;
